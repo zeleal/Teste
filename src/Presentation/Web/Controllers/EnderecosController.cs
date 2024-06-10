b0222b5d281bf536a5bffb4364c451adb45f0769
Gabriel Sanz <gabriel.sanz@infortechms.com.br>
2024-06-10 16:54:41 -0400

    Criado as controllers

---
+++ src/Presentation/Web/Controllers/EnderecosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class EnderecosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
